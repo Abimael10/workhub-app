e76807464ebc566b13bd2ccfc84c68143f624330
abimael10 <juanabimael@gmail.com>
2026-01-03 01:30:23 -0400

    feat(db): optimize connection pooling settings

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -10,6 +10,11 @@ import * as authSchema from "./schema/auth";
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
   ssl: env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined,
+  max: 20, // Maximum number of clients in the pool
+  min: 5,  // Minimum number of clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  maxUses: 7500, // Close (and replace) a connection after it has been used 7500 times
 });
 
 export const db = drizzle(pool, {
